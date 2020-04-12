321e1e7ceeeb674e992f7e95ab865386fc48efe9
Rishi Rao <22raor@gmail.com>
2020-04-12 19:11:45 -0400

    Updated course and quarters

--- src/psapi/util/Course.java
+++ src/psapi/util/Course.java
@@ -1,5 +1,81 @@
 package psapi.util;
 
+import java.util.ArrayList;
+
 public class Course {
+	public ArrayList<Quarter> quarters;
+	public String courseName;
+	public String courseTeacher;
+	public String room;
+
+	/**
+	 * @param quarters
+	 * @param courseName
+	 * @param courseTeacher
+	 * @param room
+	 */
+	public Course(ArrayList<Quarter> quarters, String courseName, String courseTeacher, String room) {
+		this.quarters = quarters;
+		this.courseName = courseName;
+		this.courseTeacher = courseTeacher;
+		this.room = room;
+	}
+
+	/**
+	 * @return the quarters
+	 */
+	public ArrayList<Quarter> getQuarters() {
+		return quarters;
+	}
+
+	/**
+	 * @param quarters the quarters to set
+	 */
+	public void setQuarters(ArrayList<Quarter> quarters) {
+		this.quarters = quarters;
+	}
+
+	/**
+	 * @return the courseName
+	 */
+	public String getCourseName() {
+		return courseName;
+	}
+
+	/**
+	 * @param courseName the courseName to set
+	 */
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	/**
+	 * @return the courseTeacher
+	 */
+	public String getCourseTeacher() {
+		return courseTeacher;
+	}
+
+	/**
+	 * @param courseTeacher the courseTeacher to set
+	 */
+	public void setCourseTeacher(String courseTeacher) {
+		this.courseTeacher = courseTeacher;
+	}
+
+	/**
+	 * @return the room
+	 */
+	public String getRoom() {
+		return room;
+	}
+
+	/**
+	 * @param room the room to set
+	 */
+	public void setRoom(String room) {
+		this.room = room;
+	}
+
 
 }
--- src/psapi/util/Quarter.java
+++ src/psapi/util/Quarter.java
@@ -1,6 +1,86 @@
 package psapi.util;
 
+import java.util.ArrayList;
+
+/**
+ * Class to represent a quarter of a course
+ * @author Rishi Rao
+ *
+ */
 public class Quarter {
-	
-	
+	private ArrayList<Assignment> assignments;
+	private double finalGrade;
+	public int quarterNum;
+
+	/**
+	 * Creates a new quarter object
+	 * @param ass
+	 * @param finalGrade
+	 * @param quarterNum
+	 */
+	public Quarter(ArrayList<Assignment> ass, double finalGrade, int quarterNum) {
+		assignments = ass;
+		this.finalGrade = finalGrade;
+		this.quarterNum = quarterNum;
+	}
+
+	/**
+	 * Add an assignment to the ArrayList of assignments
+	 * @param a
+	 */
+	public void addAssignment(Assignment a) {
+		assignments.add(a);
+	}
+
+	/**
+	 * @return the ArrayList containing all assignments in the quarter
+	 */
+	public ArrayList<Assignment> getAssignments() {
+		return assignments;
+	}
+
+	/**
+	 * @param ArrayList to replace the current assignments list
+	 */
+	public void setAssignments(ArrayList<Assignment> assignments) {
+		this.assignments = assignments;
+	}
+
+	/**
+	 * @return final grade for that quarter
+	 */
+	public double getFinalGrade() {
+		return finalGrade;
+	}
+
+	/**
+	 * @param finalGrade
+	 */
+	public void setFinalGrade(double finalGrade) {
+		this.finalGrade = finalGrade;
+	}
+
+	/**
+	 * @return the quarter number
+	 */
+	public int getQuarterNum() {
+		return quarterNum;
+	}
+
+	/**
+	 * @param quarterNum
+	 */
+	public void setQuarterNum(int quarterNum) {
+		this.quarterNum = quarterNum;
+	}
+
+	/**
+	 * @return Textual representation of quarter.
+	 */
+	@Override
+	public String toString() {
+		return "Quarter [assignments=" + assignments.toString() + ", finalGrade=" + finalGrade + ", quarterNum=" + quarterNum
+				+ "]";
+	}
+
 }
